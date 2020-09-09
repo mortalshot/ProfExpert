438787cafc4bf3d31f7d09520ccd59d357b4c0d9
mortalshot <seregagl1996@gmail.com>
2020-09-09 19:27:36 +0300

    menu links

--- ProfExpert/js/script.js
+++ ProfExpert/js/script.js
@@ -191,11 +191,33 @@ testWebP(function (support) {
 		document.querySelector('body').classList.add('no-webp');
 	}
 });
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     })
+
+
+    // Go To
+    $(".menu").on("click", ".menu__link", function (event) {
+        if (!$(this).hasClass('popup-link')) {
+            event.preventDefault();
+
+            let headerHeight = 0;
+            if ($('.header').hasClass('lock-padding')) {
+                headerHeight = parseInt($('.header').css('height'));
+            }
+
+            var id = $(this).attr('href');
+            var top = $(id).offset().top - headerHeight;
+            $('.header__burger').removeClass('active');
+            $('.header__menu').removeClass('active');
+            $('body').removeClass('lock');
+
+
+            $('body,html').animate({ scrollTop: top }, 1000);
+        }
+    });
 })
 
 
